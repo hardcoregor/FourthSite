ee5ec3e4d8e9c1746e9695d395250adcdb076e2a
hardcoregor <spraytown2014@gmail.com>
2022-09-21 22:02:25 +0300

    8/12 of work

--- Public-Gist/BTC_PRICE_BURAKOV_YEHOR.js
+++
@@ -1,9 +0,0 @@
-let priceBtc = prompt("What is Bitcoin price today?",);
-let yourDeposit = prompt("How much $ do you have?",);
-
-priceBtc = Number(priceBtc);
-yourDeposit = Number(yourDeposit);
-
-let result = yourDeposit / priceBtc;
-
-console.log(`You can buy ${result} BTC`);
